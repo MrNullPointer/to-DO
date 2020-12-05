c6a4657854076dc97b467b65a7cf2e055e9adf55
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-05 14:17:33 -0500

    New approch of printing current date added in app.js file and list.ejs file

--- app.js
+++ app.js
@@ -6,25 +6,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs"); // setup EJS
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  //StackOverflow answer: how-to-format-a-javascript-date
+  options = {
+    year: "numeric",
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  if (currentDay === 0) {
-    day = "Sunday";
-  } else if (currentDay === 1) {
-    day = "Monday";
-  } else if (currentDay === 2) {
-    day = "Tuesday";
-  } else if (currentDay === 3) {
-    day = "Wednesday";
-  } else if (currentDay === 4) {
-    day = "Thursday";
-  } else if (currentDay == 5) {
-    day = "Friday";
-  } else {
-    day = "Saturday";
-  }
+  var today = new Date();
+  day = today.toLocaleString("en-US", options);
 
   res.render("list", { kindOfDay: day });
 });
