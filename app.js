2d705e2b50f74d753023d78c2598af838f934a25
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-05 13:38:15 -0500

    Basic EJS templating functionality

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs"); // setup EJS
 
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  console.log(currentDay);
+  var day = "";
 
-  if (currentDay === 6 || currentDay === 0){
-      res.sendFile(__dirname + "/index.html")
+  if (currentDay === 0) {
+    day = "Sunday";
+  } else if (currentDay === 1) {
+    day = "Monday";
+  } else if (currentDay === 2) {
+    day = "Tuesday";
+  } else if (currentDay === 3) {
+    day = "Wednesday";
+  } else if (currentDay === 4) {
+    day = "Thursday";
+  } else if (currentDay == 5) {
+    day = "Friday";
+  } else {
+    day = "Saturday";
   }
+
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
